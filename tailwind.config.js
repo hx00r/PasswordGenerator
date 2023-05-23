67f79bcfddfea88da21ec55521314e04f24d2f0d
Hossam Mahmoud(hx00r) <mr.joker00200@gmail.com>
2023-05-23 13:07:26 +0300

    dark theme class

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
+  darkMode: "class",
   theme: {
     extend: {
       colors: {
         main: "#0F0E14",
+        "main-light": "#585f80",
         seconday: "#24232B",
         dark: "#18171F",
         "light-green": "#A4FFAF",
